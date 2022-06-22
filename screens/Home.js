421da36038e521920b53f4790f74400a59e751e4
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-06-23 04:02:22 +0900

    Home coffeeshop list

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,218 @@
-import { View, Text } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import { FlatList, View, Text, Image, ScrollView } from "react-native";
+import { ActivityIndicator } from "react-native";
 
+const SEE_COFFEESHOPS = gql`
+  query seeCoffeeShops($page: Int!) {
+    seeCoffeeShops(page: $page) {
+      id
+      name
+      latitude
+      longitude
+      user {
+        username
+      }
+      photos {
+        url
+      }
+      categories {
+        name
+      }
+    }
+  }
+`;
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
+  const [refreshing, setRefreshing] = useState(false);
+  const { data, loading, refetch, fetchMore } = useQuery(SEE_COFFEESHOPS, {
+    variables: {
+      page: 1,
+    },
+  });
+  const renderCoffeeShop = ({ item: coffeeShop }) => {
+    return (
+      <View
+        style={{ flex: 1, borderTopWidth: 1, borderBottomWidth: 1, margin: 10 }}
+      >
+        {coffeeShop.photos.length > 0 ? (
+          <ScrollView horizontal>
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+            {coffeeShop.photos.map((photo) => {
+              return (
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                    marginRight: 10,
+                    backgroundColor: "grey",
+                  }}
+                  source={photo.url}
+                />
+              );
+            })}
+          </ScrollView>
+        ) : null}
+        <Text style={{ fontSize: 25 }}>{coffeeShop.name}</Text>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          {coffeeShop.categories.map(({ name }) => {
+            return (
+              <View
+                style={{
+                  marginHorizontal: 5,
+                  paddingHorizontal: 15,
+                  backgroundColor: "black",
+                }}
+              >
+                <Text style={{ color: "white" }}>{name}</Text>
+              </View>
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
+  const refresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+  console.log(data);
+  return loading ? (
+    <ActivityIndicator color="white" />
+  ) : (
+    <FlatList
+      onEndReachedThreshold={0.02}
+      onEndReached={() =>
+        fetchMore({
+          variables: {
+            page: 1 + (data?.seeCoffeeShops?.length || 0) / 5,
+          },
+        })
+      }
+      refreshing={refreshing}
+      onRefresh={refresh}
+      style={{ width: "100%" }}
+      showsVerticalScrollIndicator={false}
+      data={data?.seeCoffeeShops}
+      keyExtractor={(photo) => "" + photo.id}
+      renderItem={renderCoffeeShop}
+    />
   );
 };
 
